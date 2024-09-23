3fb8ed0544eb17c448e58c08a8c0c77d8043255b
Walther Awad <waltherawad@gmail.com>
2024-09-23 16:56:19 -0400

    Se comienzaa maquetar y a crear componentes
    - Se crean los siguientes componentes globales:
        - CustomIcon
        - CustomTooltip
    - Se crea el componente CardSummary dentro de "./app/(routes)/components
    
    -Tambien se instalo componente tooltip de Shadcn

---
+++ components/CustomTooltip/CustomTooltip.tsx
@@ -0,0 +1,20 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
+import { CustomTooltipProps } from './CustomTooltip.types';
+import { Info } from 'lucide-react';
+
+export function CustomTooltip(props: CustomTooltipProps) {
+	const { content } = props;
+	return (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger>
+					<Info strokeWidth={1} className="w-5 h-5" />
+				</TooltipTrigger>
+				<TooltipContent>
+					<p>{content}</p>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
+}
