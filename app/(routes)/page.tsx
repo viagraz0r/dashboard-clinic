f1ff97a09b1f7193cc50cd707a02bd6ffe03d29b
Walther Awad <waltherawad@gmail.com>
2024-09-23 15:48:46 -0400

    Se instala componente Button de shadcn "npx shadcn@latest add button"

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div>Home</div>
+      
       <UserButton />
+      <h2 className="text-2xl mb-4">Dashboard</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+      </div>
     </div>
   );
 }
