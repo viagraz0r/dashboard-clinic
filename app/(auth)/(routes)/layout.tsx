2be80ae5ccdd133953c2d1ff5be36378e2daa93e
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-09-24 15:16:15 -0400

    Se pone en funcionamiento ToggleTheme
    - Se instala theme-provider de shadcn
    - se instala ui component dropdown-menu, requerido por el ToggleTheme
    
    - ademas de colocar el componente de Logo en el layout de Auth

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,11 +1,12 @@
+import { Logo } from '@/components/Logo';
+
 export default function LayoutAuth({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex flex-col justify-center h-full items-center">
-            <p>Dashboard Clinic</p>
-            <h1 className="text-3xl my-2">
-                Welcome!
-            </h1>
-            { children }
-        </div>
-    )
+	return (
+		<div className="flex flex-col justify-center h-full items-center">
+			<Logo />
+			<h1 className="text-3xl my-2">Welcome to the Dashboard!</h1>
+			<h2 className="text-2xl mb-3">zorcode.com</h2>
+			{children}
+		</div>
+	);
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
+import { Menu, Search } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+
 import { Input } from '@/components/ui/input';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { UserButton } from '@clerk/nextjs';
-import { Menu, Search } from 'lucide-react';
-import { SidebarRoutes } from '../SidebarRoutes';
+
+import { SidebarRoutes } from '@/components/SidebarRoutes';
+import { ToggleTheme } from '@/components/ToggleTheme';
 
 export function Navbar() {
 	return (
-		<div className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
+		<nav className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
 			<div className="block xl:hidden">
 				<Sheet>
 					<SheetTrigger className="flex items-center">
@@ -22,9 +25,9 @@ export function Navbar() {
 				<Search strokeWidth={1} className="absolute top-2 right-2" />
 			</div>
 			<div className="flex gap-x-2 items-center">
-				<p>Toogletheme</p>
+				<ToggleTheme />
 				<UserButton />
 			</div>
-		</div>
+		</nav>
 	);
 }
