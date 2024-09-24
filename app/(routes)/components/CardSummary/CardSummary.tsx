8d547164293326894c46fb14111e6cfcd5a11806
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-09-23 21:32:10 -0400

    Se agregan los datos a mostrar en CardSummary

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,6 +1,8 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 import { CustomTooltip } from "@/components/CustomTooltip";
+import { cn } from "@/lib/utils";
+import { MoveDownRight, MoveUpRight, TrendingUp } from "lucide-react";
 
 export function CardSummary(props: CardSummaryProps) {
   const { average, icon: Icon, title, tooltipText, total } = props;
@@ -12,6 +14,25 @@ export function CardSummary(props: CardSummaryProps) {
           {title}
         </div>
         <CustomTooltip content={tooltipText} />
+      </div>
+      <div className="flex gap-4 mt-2 md:mt-4">
+        <p className="text-2xl">{total}</p>
+        <div
+          className={cn(
+            `flex items-center gap-1 text-xs px-2 text-white rounded-lg h-[20px] bg-black dark:bg-secondary`
+          )}
+        >
+          {average}%
+          {average < 20 && (
+            <MoveDownRight strokeWidth={2} className="w-4 h-4" />
+          )}
+          {average > 20 && average < 70 && (
+            <MoveUpRight strokeWidth={2} className="w-4 h-4" />
+          )}
+          {average > 70 && average < 100 && (
+            <TrendingUp strokeWidth={2} className="w-4 h-4" />
+          )}
+        </div>
       </div>
     </div>
   );
