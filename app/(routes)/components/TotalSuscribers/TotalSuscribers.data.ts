b7efb03d59efcd06380eab3fcdebb83683a460be
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-09-26 20:43:57 -0400

    Se crea y añade componente de TotalSuscribers

---
+++ app/(routes)/components/TotalSuscribers/TotalSuscribers.data.ts
@@ -0,0 +1,17 @@
+export const dataTotalSuscribers = [
+  {
+    name: "Website",
+    value: 456,
+    fill: "#8884d8",
+  },
+  {
+    name: "Instagram",
+    value: 854,
+    fill: "#00c49f",
+  },
+  {
+    name: "Other",
+    value: 240,
+    fill: "#ffbb28",
+  },
+];
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
-import { CardSummary } from "./components/CardSummary";
 import { BookOpenCheck, UsersRound, Waypoints } from "lucide-react";
+import { CardSummary } from "./components/CardSummary";
 import { LastCustomers } from "./components/LastCustomers";
 import { SalesDistributors } from "./components/SalesDistributors";
-import { CustomersTable } from "./components/CustomersTable/CustomersTable";
+import { TotalSuscribers } from "./components/TotalSuscribers";
 
 export const dataCardsSummary = [
   {
@@ -50,6 +49,10 @@ export default function Home() {
       <div className="grid grid-cols-1 xl:grid-cols-2 md:gap-x-10 mt-12">
         <LastCustomers />
         <SalesDistributors />
+      </div>
+      <div className="flex-col xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 mb-10 justify-center">
+        <TotalSuscribers />
+        <p>List Integrations</p>
       </div>
     </div>
   );
