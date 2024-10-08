d3338ea5652db6e2a4cfbabb495977b2e5d8489d
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-10-08 15:38:27 -0400

    Se termina el formulario para la creacion de clientes
    Se instalan los components uploadthing y toast utilizados para
    la subida de archivos al servicio cloud uploadthing.com

--- app/(routes)/companies/components/FormCreateCustomer/FormCreateCustomer.tsx
+++ app/(routes)/companies/components/FormCreateCustomer/FormCreateCustomer.tsx
@@ -1,160 +1,175 @@
-"use client";
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
-import { z } from "zod";
+import { z } from 'zod';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { FormCreateCustomerProps } from "./FormCreateCustomer.types";
-import { useState } from "react";
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { FormCreateCustomerProps } from './FormCreateCustomer.types';
+import { useState } from 'react';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { UploadButton } from '@/utils/uploadthing';
+import { toast } from '@/hooks/use-toast';
 
 const formSchema = z.object({
-  name: z.string(),
-  country: z.string().min(2),
-  website: z.string().min(2),
-  phone: z.string().min(11),
-  ci: z.string().min(8),
-  profileImage: z.string(),
+	name: z.string(),
+	country: z.string().min(2),
+	website: z.string().min(2),
+	phone: z.string().min(11),
+	ci: z.string().min(8),
+	profileImage: z.string(),
 });
 
 export function FormCreateCustomer(props: FormCreateCustomerProps) {
-  const { setOpenModalCreate } = props;
-  const [photoUploaded, setPhotouploaded] = useState(false);
+	const { setOpenModalCreate } = props;
+	const [photoUploaded, setPhotouploaded] = useState(false);
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      country: "",
-      website: "",
-      phone: "",
-      ci: "",
-      profileImage: "",
-    },
-  });
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: '',
+			country: '',
+			website: '',
+			phone: '',
+			ci: '',
+			profileImage: '',
+		},
+	});
 
-  const { isValid } = form.formState;
+	const { isValid } = form.formState;
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-  };
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
 
-  return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="grid grid-cols-2 gap-3">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Company name</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Name of company here..."
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="country"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Country</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select the country" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="united-kingdom">
-                        United Kingdom
-                      </SelectItem>
-                      <SelectItem value="spain">Spain</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="website"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Website</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="http://hostname.com"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Phone</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="04XX-XXXXXXX"
-                      type="number"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="ci"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>CI</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Cedula here..."
-                      type="number"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
-  );
+	return (
+		<div>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<div className="grid grid-cols-2 gap-3">
+						<FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Company name</FormLabel>
+									<FormControl>
+										<Input placeholder="Name of company here..." type="text" {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="country"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Country</FormLabel>
+									<Select onValueChange={field.onChange} defaultValue={field.value}>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue placeholder="Select the country" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											<SelectItem value="united-kingdom">United Kingdom</SelectItem>
+											<SelectItem value="spain">Spain</SelectItem>
+										</SelectContent>
+									</Select>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="website"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Website</FormLabel>
+									<FormControl>
+										<Input placeholder="http://hostname.com" type="text" {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="phone"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Phone</FormLabel>
+									<FormControl>
+										<Input placeholder="04XX-XXXXXXX" type="number" {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="ci"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>CI</FormLabel>
+									<FormControl>
+										<Input placeholder="Cedula here..." type="number" {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="profileImage"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Profile Image</FormLabel>
+									<FormControl>
+										{photoUploaded ? (
+											<p className="text-sm">Image uploaded!</p>
+										) : (
+											<UploadButton
+												className="rounded-lg bg-slate-600/20 text-slate-800 outline-dotted outline-3"
+												{...field}
+												endpoint="profileImage"
+												onClientUploadComplete={(res) => {
+													form.setValue('profileImage', res?.[0].url);
+													toast({
+														title: 'Photo uploaded!',
+													});
+													setPhotouploaded(true);
+												}}
+												onUploadError={(error: Error) => {
+													toast({
+														title: 'Error uploading photo',
+													});
+												}}
+											/>
+										)}
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					</div>
+					<Button type="submit" disabled={!isValid}>
+						Submit
+					</Button>
+				</form>
+			</Form>
+		</div>
+	);
 }
